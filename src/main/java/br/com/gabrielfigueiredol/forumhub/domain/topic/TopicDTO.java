f9c3e07021d714fa2b37e897ca29f339f788add2
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-12-12 18:14:13 -0300

    refactor: add topic id

--- src/main/java/br/com/gabrielfigueiredol/forumhub/domain/topic/TopicDTO.java
+++ src/main/java/br/com/gabrielfigueiredol/forumhub/domain/topic/TopicDTO.java
@@ -4,6 +4,7 @@ import br.com.gabrielfigueiredol.forumhub.domain.course.CourseDTO;
 import java.time.LocalDateTime;
 
 public record TopicDTO(
+        Long id,
         String title,
         String message,
         LocalDateTime createdAt,
@@ -12,6 +13,6 @@ public record TopicDTO(
         Long user_id
 ) {
     public TopicDTO(Topic topic) {
-        this(topic.getTitle(), topic.getMessage(), topic.getCreatedAt(), topic.getOpenTopic(), new CourseDTO(topic.getCourse()), topic.getUser().getId());
+        this(topic.getId(), topic.getTitle(), topic.getMessage(), topic.getCreatedAt(), topic.getOpenTopic(), new CourseDTO(topic.getCourse()), topic.getUser().getId());
     }
 }
